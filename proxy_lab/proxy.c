b8911ae3a561abd10a3ad1041287b086cfeeec04
junjieliu2910 <junjieliu2910@gmail.com>
2018-05-11 21:58:42 +0800

    cache part

--- proxy_lab/proxy.c
+++ proxy_lab/proxy.c
@@ -5,7 +5,8 @@
 /* Recommended max cache and object sizes */
 #define MAX_CACHE_SIZE 1049000
 #define MAX_OBJECT_SIZE 102400
-
+#define MAX_THREAD 50
+#define CACHE 3
 
 /* You won't lose style points for including this long line in your code */
 static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
@@ -20,12 +21,23 @@ static const char *user_agent_key= "User-Agent";
 static const char *proxy_connection_key = "Proxy-Connection";
 static const char *host_key = "Host";
 
+typedef struct {
+    char uri[MAXLINE];
+    struct Cache_file* next;
+    int length;
+}Cache_file;
+
+sem_t LOG, CACHE;
+struct Cache_file* CACHE_FILE;
+
 void client_error(int fd, char *cause, char*errnum, char* shortmsg, char* longmsg);
 int connect_to_endserver(char* hostname, int port);
 void parse_uri(char *uri,char *hostname,char *path,int *port);
 void build_http_header(char *http_header,char *hostname,char *path,int port,rio_t *client_rio);
-void format_log_entry(char *logstring, struct sockaddr_in *sockaddr, char *uri, int size);
-void proxy(int conn_fd);
+void format_log_entry(char *logstring, int conn_fd, char *uri, int size);
+void* proxy(void* conn_fd);
+int check_cache(char *uri);
+void add_cache(char* uri, int length);
 
 
 int main(int argc, char *argv[])
@@ -43,22 +55,32 @@ int main(int argc, char *argv[])
         fprintf(stderr, "Open listen port error:");
         exit(EXIT_FAILURE);
     }
+    sem_init(&LOG, 0, 1);
+    sem_init(&CACHE, 0, 1);
+    //cache opeartion
+    cache = malloc(CACHE*sizeof(Cache_file));
+    for(int i = 0; i < CACHE; ++i){
+        CACHE_FILE[i].next=NULL;
+    }
+
+
     while(1){
         client_len = sizeof(client_addr);
         conn_fd = Accept(listen_fd, (SA* )&client_addr, &client_len);
         Getnameinfo((SA*)&client_addr, client_len, client_hostname, MAXLINE, client_port, MAXLINE, 0);
         printf("Accepted connection from (%s, %s)\n", client_hostname, client_port);
-        proxy(conn_fd);
-        Close(conn_fd);
+        pthread_t tid;
+        Pthread_create(&tid, NULL, proxy, &conn_fd);
     }
-
     //printf("%s", user_agent_hdr);
     return 0;
 }
 
-void proxy(int conn_fd){
+void* proxy(void* fd){
     // Io variables
     size_t n;
+    int conn_fd = *((int *)fd);
+    Pthread_detach(pthread_self());
     rio_t rio, server_rio; // server_rio to receive the bytes from end server
     char buf[MAXLINE];
     char method[MAXLINE], url[MAXLINE], version[MAXLINE];
@@ -70,31 +92,91 @@ void proxy(int conn_fd){
 
     if(strcasecmp(method, "GET")){
         client_error(conn_fd, method, "501", "Not implemented", "Tiny does not implement this method");
-        return;
+        return -1;
     }
 
+    //cache operation
+    int check_cache = -1, length = 0;
+    check_cache = check_cache(url);
 
-    char hostname[MAXLINE], pathname[MAXLINE], end_server_http_header[MAXLINE];
-    int proxy_conn_fd, port;
-    parse_uri(url, hostname, pathname, &port);
-    printf("Hostname: %s, port:%d, pathname: %s\n", hostname, port, pathname);
-    build_http_header(end_server_http_header, hostname, pathname, port, &rio);
 
-    if((proxy_conn_fd = connect_to_endserver(hostname, port)) < 0){
-        fprintf(stderr, "proxy connect error");
-        return;
+    if(check_cache){
+
+    }else{
+        char hostname[MAXLINE], pathname[MAXLINE], end_server_http_header[MAXLINE];
+        int proxy_conn_fd, port;
+        parse_uri(url, hostname, pathname, &port);
+        build_http_header(end_server_http_header, hostname, pathname, port, &rio);
+
+        if((proxy_conn_fd = connect_to_endserver(hostname, port)) < 0){
+            fprintf(stderr, "proxy connect error");
+            return -2;
+        }
+        Rio_writen(proxy_conn_fd,end_server_http_header,strlen(end_server_http_header));
+        char key[MAXLINE], value[MAXLINE], content_length[MAXLINE];
+        Rio_readinitb(&server_rio, proxy_conn_fd);
+        //Read the status line
+        if((n = Rio_readlineb(&server_rio, buf, MAXLINE)) > 0){
+            Rio_writen(conn_fd, buf, strlen(buf));
+        }
+        //Read and transmit the response header
+        while((n = Rio_readlineb(&server_rio, buf, MAXLINE)) > 0 && strcmp(buf, "\r\n")){
+            //printf("%s", buf);
+            sscanf(buf, "%s %s", key, value);
+            //printf("%s, %s\n", key, value);
+            if(!strcmp(key, "Content-length:")){
+                strcpy(content_length, value);
+                length = atoi(content_length);
+                //printf("The Content-length is %s\n", content_length);
+            }
+            Rio_writen(conn_fd, buf, strlen(buf));
+        }
+        Rio_writen(conn_fd, buf, strlen(buf)); // write /r/n to the conn_fd
+
+        while((n = Rio_readlineb(&server_rio, buf, MAXBUF)) > 0){
+            //printf("Proxy server receive %d bytes\n", (int)n);
+            Rio_writen(conn_fd, buf, (int)n);
+        }
+
+        P(&CACHE);
+        check_cache = cache_add(url);
+        V(&CACHE);
     }
 
-    Rio_writen(proxy_conn_fd,end_server_http_header,strlen(end_server_http_header));
+    char log_string[MAXLINE];
+    format_log_entry(log_string, conn_fd, url, length);
 
-    Rio_readinitb(&server_rio, proxy_conn_fd);
-    while((n = Rio_readlineb(&server_rio, buf, MAXLINE)) > 0){
-        printf("Proxy server receive %d bytes\n", (int)n);
-        Rio_writen(conn_fd, buf, strlen(buf));
+    //record log
+    P(&LOG);
+    FILE* log = Fopen("proxy.log", "a");
+    Fwrite(log_string, sizeof(char), strlen(log_string), log);
+    Fclose(log);
+    V(&LOG);
+
+
+
+    printf("%s", log_string);
+    Close(conn_fd);
+    return 0;
+}
+
+void add_cache(char* uri, int length){
+    struct Cache_file* tmp = CACHE_FILE;
+    P(&CACHE);
+    V(&CACHE);
+}
+
+int check_cache(char* uri){
+    struct Cache_file* tmp = CACHE_FILE;
+    P(&CACHE);
+    for(; tmp->next !=NULL; tmp = tmp->next){
+        if(!strcmp(tmp->uri, uri)){
+            V(&CACHE);
+            return 1;
+        }
     }
-
-    return;
-
+    V(&CACHE);
+    return 0;
 }
 
 inline int connect_to_endserver(char* hostname, int port){
@@ -190,12 +272,10 @@ void client_error(int fd, char *cause, char*errnum, char* shortmsg, char* longms
     Rio_writen(fd, body, strlen(body));
 }
 
-void format_log_entry(char *logstring, struct sockaddr_in *sockaddr,
-		      char *uri, int size){
+void format_log_entry(char *logstring, int conn_fd, char *uri, int size){
     time_t now;
     char time_str[MAXLINE];
-    unsigned long host;
-    unsigned char a, b, c, d;
+    // /unsigned long host;
 
     /* Get a formatted time string */
     now = time(NULL);
@@ -207,13 +287,13 @@ void format_log_entry(char *logstring, struct sockaddr_in *sockaddr,
      * because inet_ntoa is a Class 3 thread unsafe function that
      * returns a pointer to a static variable (Ch 13, CS:APP).
      */
-    host = ntohl(sockaddr->sin_addr.s_addr);
-    a = host >> 24;
-    b = (host >> 16) & 0xff;
-    c = (host >> 8) & 0xff;
-    d = host & 0xff;
-
+     struct sockaddr_in addr;
+     socklen_t addr_size = sizeof(struct sockaddr_in);
+     getpeername(conn_fd, (struct sockaddr *)&addr, &addr_size);
+     char clientip[100];
+     strcpy(clientip, inet_ntoa(addr.sin_addr));
 
     /* Return the formatted log entry string */
-    sprintf(logstring, "%s: %d.%d.%d.%d %s %d", time_str, a, b, c, d, uri, size);
+    sprintf(logstring, "%s: %s %s %d\n", time_str, clientip, uri, size);
+    return;
 }
